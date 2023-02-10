9036b9dcab076d8f957df4783e3b0152059f4fd7
Jacob Studstill <jacob.studstill@gmail.com>
2023-02-10 16:27:38 -0500

    Changed title/h1 name and added Home and Dashboard button

--- models/index.js
+++ models/index.js
@@ -10,4 +10,11 @@ Posts.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+//Posts has many comments
+
+//User has many comments
+
+//Comment belongs to post
+//Comment belongs to user
+
 module.exports = { User, Posts };
